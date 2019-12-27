3471b51e1c71c882c0cd30a728e2778b8631b35d
kapilkulkarni25 <59175671+kapilkulkarni25@users.noreply.github.com>
2019-12-27 15:05:22 +0530

    Add files via upload

---
+++ al.js
@@ -0,0 +1,16 @@
+$(document).ready(
+    function()
+    {
+       // $("p").text("AA");
+       // $("p").css({"color":"Red","background-color":"Blue"});
+      //  attr("","color","Green");
+      $("a").val();
+      $("#bt").click(
+          function()
+          {
+              $("h1").text($("#a").val());
+          }
+      )
+        alert("Hey");
+    }
+)
