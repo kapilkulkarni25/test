aa6483b006506858eab9a5eda460b767a3ba4e70
kapilkulkarni25 <59175671+kapilkulkarni25@users.noreply.github.com>
2019-12-27 16:34:48 +0530

    Add files via upload

--- al.js
+++ al.js
@@ -1,16 +1,35 @@
 $(document).ready(
     function()
     {
+        $("input").focus(
+            function()
+            {
+                $(this).css("background-color","Blue")
+            }
+        )
+        $("input").blur(
+            function()
+            {
+                $(this).css("background-color","white")
+            }
+        )
+        
        // $("p").text("AA");
        // $("p").css({"color":"Red","background-color":"Blue"});
       //  attr("","color","Green");
-      $("a").val();
-      $("#bt").click(
-          function()
-          {
-              $("h1").text($("#a").val());
-          }
-      )
-        alert("Hey");
+    //   $("#a").val();
+    //   $("#a").focus(
+    //       function()
+    //       {
+    //           $("#a").css("background-color","Blue")
+    //       }
+    //   )
+    //   $("#a").blur(
+    //       function()
+    //       {
+    //         $("#a").css("background-color","white")
+    //       }
+    //   )
+    //     alert("Hey");
     }
 )
