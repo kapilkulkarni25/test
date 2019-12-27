2f224a32e23e7119d9de855287c23c2782710d15
kapilkulkarni25 <59175671+kapilkulkarni25@users.noreply.github.com>
2019-12-27 13:28:11 +0530

    Add files via upload

--- ex.js
+++ ex.js
@@ -1,6 +1,63 @@
-function alrt(){
-//  var userValue = document.getElementById("a").value;
-//     // var b = document.getElementById("b").value;
+function vowel()
+{
+        var kap = document.getElementById("a").value;
+        var countA=0;
+        var countE=0;
+        var countI=0;
+        var countO=0;
+        var countU=0;
+        var str="";
+        var userValue = kap.toLowerCase();
+        for(var i=userValue.length-1;i>=0;i--)
+        {
+            str=str+userValue[i];
+
+        if(userValue[i]=="a")
+        countA++;
+        else if(userValue[i]=="u")
+        countU++;
+        else if(userValue[i]=="e")
+        countE;
+        else if(userValue[i]=="o")
+        countO++;
+        else if(userValue[i]=="u")
+        countU++;
+
+        }
+        // for(var i=0;i<userValue.length-1;i++)
+        // {
+        // if(userValue[i]=="a" || userValue[i]=="A")
+        // {
+        // countA++
+        // }
+        // else if (userValue[i]=="e" || userValue[i]=="E")
+        // {
+        //     countE++
+        // }
+        // else if(userValue[i]=="i" || userValue[i]=="I")
+        // {
+        //     countI++
+        // }else
+        // if(userValue[i]=="o" || userValue[i]=="O")
+        // {
+        //     countO++
+        // }else
+        // if(userValue[i]=="u" || userValue[i]=="U")
+        // {
+        //     countU++
+        // }
+        document.getElementById("Rev").innerHTML="Reverse="+str;
+    
+        document.getElementById("out").innerHTML="A="+countA;
+        document.getElementById("E").innerHTML=" E="+countE;
+        document.getElementById("I").innerHTML=" I="+countA;
+        document.getElementById("O").innerHTML=" O="+countO;
+        document.getElementById("U").innerHTML=" U="+countU;
+
+        }
+
+
+//      var b = document.getElementById("b").value;
 
 //     var namePattern="^[A-Za-z]+$"
 //     // document.getElementById("image").src = "https://images-na.ssl-images-amazon.com/images/I/91cy-FkSK6L._SX355_.jpg"
@@ -9,17 +66,18 @@ function alrt(){
 //     document.getElementById("errorname").innerHTML="Name should be in alphabets"
 //     else
 //     document.getElementById("errorname").innerHTML=""
-var ele =document.getElementById("stat").value
-if(ele == "NONE"){
-document.getElementById("err").innerHTML="Please Select a State"
-return false
-}
-else 
-{
-    return true
+// var ele =document.getElementById("stat").value
 
-}
+// if(ele == "NONE"){
+// document.getElementById("err").innerHTML="Please Select a State"
+// return false
+// }
+// else 
+// {
+//     return true
+
+// }
 
 
     
-}
+
